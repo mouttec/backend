d1dc87616add38577d1a8d9a7c86b968793b0036
Yann <yannick@wolfpack-edition.com>
2021-06-04 18:01:57 +0300

    Ajout des headers en php

--- backend/api/teammate/listTeammates.php
+++ backend/api/teammate/listTeammates.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-With");	
 include_once "../../config/Database.php";
 include_once "../../models/Teammate.php";
 // include_once "../../models/Agency.php";
--- backend/api/teammate/loginTeammate.php
+++ backend/api/teammate/loginTeammate.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-With");
 include_once("../../config/database.php");
 include_once "../../models/Teammate.php";
 
