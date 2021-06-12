385dba2230624a2fd84f35bb937f72a36043fd29
Yann <yannick@wolfpack-edition.com>
2021-06-12 17:52:32 +0300

    Rerecorrection

--- backend/api/booking/editBookingByPartner.php
+++ backend/api/booking/editBookingByPartner.php
@@ -39,7 +39,7 @@ if (empty($decodedData->idCustomer)) {
         $randomStr .= $chars[rand(0, $maxLength - 1)];
     }
     $customer->mixedPassword = $randomStr;
-    $customer->idBillingAddress = 'N/A';
+    $customer->idBillingAddress = '0';
     $customer->createCustomer($customer);
     //idCustomer créée
     $thisCustomer = new Customer($conn);
