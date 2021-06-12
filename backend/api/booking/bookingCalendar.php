7e2dbdb3ff08b0095e3afecd559a47f7565c3b5e
Yann <yannick@wolfpack-edition.com>
2021-06-12 21:22:35 +0300

    Test calendar

--- backend/api/booking/bookingCalendar.php
+++ backend/api/booking/bookingCalendar.php
@@ -32,52 +32,62 @@ if (isset($decodedData->idAgency)) {
     $driversInAgency = 1;
 }
 
-$quarters = ['00', '15', '30', '45'];
-$shifts = array(); 
-for ($h = 7; $h <= 20; $h++) {
-    foreach ($quarters as $quarter) {
-        if (strlen($h) == 1) {
-            $timecode = '0'.$h.':'.$quarter;
-        } else {
-            $timecode = $h.':'.$quarter;
-        }
-        //On retire les créaneaux non disponibles dans le calendrier
-        if (($timecode != '07:00') && ($timecode != '07:15') && ($timecode != '20:30') && ($timecode != '20:30')) {
-            $shifts[$timecode] = $driversInAgency;
-        }
-    }
-}
+// $quarters = ['00', '15', '30', '45'];
+// $shifts = array(); 
+// for ($h = 7; $h <= 20; $h++) {
+//     foreach ($quarters as $quarter) {
+//         if (strlen($h) == 1) {
+//             $timecode = '0'.$h.':'.$quarter;
+//         } else {
+//             $timecode = $h.':'.$quarter;
+//         }
+//         //On retire les créaneaux non disponibles dans le calendrier
+//         if (($timecode != '07:00') && ($timecode != '07:15') && ($timecode != '20:30') && ($timecode != '20:45)) {
+//             $shifts[$timecode] = $driversInAgency;
+//         }
+//     }
+// }
+
+$shiftsAvailable = ['07:30', '08:30', '09:30', '10:30', '11:30', '13:30', '14:30', '15:30', '16:30', '17:30', '18:30'];
 
 //Création des lignes jours dans le calendrier, remplie chacune avec les shifts
 $calendar = array();
 for ($i = 0; $i < 60; $i++) {
-    $calendar[date('Y-m-d', strtotime('+'.$i.' days'))] = $shifts;
+    $day = array();
+    $day = [
+        "idBookingCalendar" => $i+1,
+        "dateBookingCalendar" => date('Y-m-d', strtotime('+'.$i.' days')),
+    ];
+    foreach ($$shiftsAvailable as $key => $shift) {
+        $day['h'.$key+1.'bookingCalendar'] = $shift.'-'.date('Y-m-d', strtotime('+'.$i.' days'))
+    }
+    array_push($calendar, $day);
 }
 
-$booking->idAgency = $decodedData->idAgency;
-$bookings = $booking->searchBookingsForCalendar($booking);
-$counter = $bookings->rowCount();
-if ($counter > 0) {
-    while ($row = $bookings->fetch()) {
-        extract($row);
-        if (!is_null($dateForth)) {
-            //$dateForth prend le format YYYY-MM-DD
-            $dateForth =  implode('-', array_reverse(explode('/', $dateForth)));
-            //$durationDelayInQuarters correspond au nombre de quart d'heure que prend la presta
-            $durationDelayInQuarters = round(($durationForth+20)/15);
-            for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
-                $calendar[$dateForth][date('H:i', strtotime($hoursForth.' +'.$i*15.' minutes'))] -= 1;
-            }
-        }
-        if (!is_null($dateBack)) {
-            $dateBack =  implode('-', array_reverse(explode('/', $dateBack)));
-            $durationDelayInQuarters = round(($durationBack+20)/15);
-            for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
-                $calendar[$dateBack][date('H:i', strtotime($hoursBack.' +'.$i*15.' minutes'))] -= 1;
-            }
-        }
-    }
-}
+// $booking->idAgency = $decodedData->idAgency;
+// $bookings = $booking->searchBookingsForCalendar($booking);
+// $counter = $bookings->rowCount();
+// if ($counter > 0) {
+//     while ($row = $bookings->fetch()) {
+//         extract($row);
+//         if (!is_null($dateForth)) {
+//             //On donne à $dateForth le format YYYY-MM-DD
+//             $dateForth =  implode('-', array_reverse(explode('/', $dateForth)));
+//             //$durationDelayInQuarters correspond au nombre de quart d'heure que prend la presta
+//             $durationDelayInQuarters = round(($durationForth+20)/15);
+//             for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
+//                 $calendar[$dateForth][date('H:i', strtotime($hoursForth.' +'.$i*15.' minutes'))] -= 1;
+//             }
+//         }
+//         if (!is_null($dateBack)) {
+//             $dateBack =  implode('-', array_reverse(explode('/', $dateBack)));
+//             $durationDelayInQuarters = round(($durationBack+20)/15);
+//             for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
+//                 $calendar[$dateBack][date('H:i', strtotime($hoursBack.' +'.$i*15.' minutes'))] -= 1;
+//             }
+//         }
+//     }
+// }
 
 if (isset($calendar) && !empty($calendar)) {
     echo json_encode($calendar);
