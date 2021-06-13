bfdeb9da905ffd2dd9ebc92ced199a001afe4dac
Yann <yannick@wolfpack-edition.com>
2021-06-13 07:38:09 +0300

    Modification génération du calendrier

--- backend/api/booking/bookingCalendar.php
+++ backend/api/booking/bookingCalendar.php
@@ -15,22 +15,22 @@ $teammate = new Teammate($conn);
 $decodedData = json_decode(file_get_contents("php://input"));
 
 //Recherche du nombre de teammates "drivers" disponibles dans l'agence concernée 
-if (isset($decodedData->idAgency)) {
-    $teammate->idAgency = $decodedData->idAgency;
-    $teammates = $teammate->searchTeammatesByAgency($teammate);
-    $counter = $teammates->rowCount();
-    if ($counter > 0) {
-        $driversInAgency = 0;
-        while($row = $teammates->fetch()) {
-            extract($row);
-            if (($jobTeammate == 'driver') && ($statusTeammate == 1)) {
-                $driversInAgency += 1;
-            }
-        }
-    }
-} else {
-    $driversInAgency = 1;
-}
+// if (isset($decodedData->idAgency)) {
+//     $teammate->idAgency = $decodedData->idAgency;
+//     $teammates = $teammate->searchTeammatesByAgency($teammate);
+//     $counter = $teammates->rowCount();
+//     if ($counter > 0) {
+//         $driversInAgency = 0;
+//         while($row = $teammates->fetch()) {
+//             extract($row);
+//             if (($jobTeammate == 'driver') && ($statusTeammate == 1)) {
+//                 $driversInAgency += 1;
+//             }
+//         }
+//     }
+// } else {
+//     $driversInAgency = 1;
+// }
 
 // $quarters = ['00', '15', '30', '45'];
 // $shifts = array(); 
@@ -42,15 +42,29 @@ if (isset($decodedData->idAgency)) {
 //             $timecode = $h.':'.$quarter;
 //         }
 //         //On retire les créaneaux non disponibles dans le calendrier
-//         if (($timecode != '07:00') && ($timecode != '07:15') && ($timecode != '20:30') && ($timecode != '20:45)) {
+//         if (($timecode != '07:00') && ($timecode != '07:15') && ($timecode != '20:30') && ($timecode != '20:45')) {
 //             $shifts[$timecode] = $driversInAgency;
 //         }
 //     }
 // }
 
-$shiftsAvailable = ['07:30', '08:30', '09:30', '10:30', '11:30', '13:30', '14:30', '15:30', '16:30', '17:30', '18:30'];
+$quarters = ['30'];
+$shiftsAvailable = array(); 
+//Modification des plages d'heures : $h = heure mini, $h <= heure maxi
+for ($h = 7; $h <= 18; $h++) {
+    foreach ($quarters as $quarter) {
+        if (strlen($h) == 1) {
+            $shift = '0'.$h.':'.$quarter;
+        } else {
+            $shift = $h.':'.$quarter;
+        }
+        //On retire les créaneaux non disponibles dans le calendrier
+        if ($shift != '12:30') {
+           array_push($shiftsAvailable, $shift);
+        }
+    }
+}
 
-//Création des lignes jours dans le calendrier, remplie chacune avec les shifts
 $calendar = array();
 for ($i = 0; $i < 60; $i++) {
     $day = array();
@@ -58,36 +72,43 @@ for ($i = 0; $i < 60; $i++) {
         "idBookingCalendar" => $i+1,
         "dateBookingCalendar" => date('Y-m-d', strtotime('+'.$i.' days')),
     ];
-    foreach ($$shiftsAvailable as $key => $shift) {
-        $day['h'.($key+1).'bookingCalendar'] = $shift.'-'.date('Y-m-d', strtotime('+'.$i.' days'))
+    foreach ($shiftsAvailable as $key => $shift) {
+        $day['h'.($key+1).'bookingCalendar'] = $shift.'-'.date('Y-m-d', strtotime('+'.$i.' days'));
     }
     array_push($calendar, $day);
 }
 
-// $booking->idAgency = $decodedData->idAgency;
-// $bookings = $booking->searchBookingsForCalendar($booking);
-// $counter = $bookings->rowCount();
-// if ($counter > 0) {
-//     while ($row = $bookings->fetch()) {
-//         extract($row);
-//         if (!is_null($dateForth)) {
-//             //On donne à $dateForth le format YYYY-MM-DD
-//             $dateForth =  implode('-', array_reverse(explode('/', $dateForth)));
-//             //$durationDelayInQuarters correspond au nombre de quart d'heure que prend la presta
-//             $durationDelayInQuarters = round(($durationForth+20)/15);
-//             for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
-//                 $calendar[$dateForth][date('H:i', strtotime($hoursForth.' +'.$i*15.' minutes'))] -= 1;
-//             }
-//         }
-//         if (!is_null($dateBack)) {
-//             $dateBack =  implode('-', array_reverse(explode('/', $dateBack)));
-//             $durationDelayInQuarters = round(($durationBack+20)/15);
-//             for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
-//                 $calendar[$dateBack][date('H:i', strtotime($hoursBack.' +'.$i*15.' minutes'))] -= 1;
-//             }
-//         }
-//     }
-// }
+// "h4bookingCalendar": "10:30-2021-06-10",
+
+$booking->idAgency = $decodedData->idAgency;
+$bookings = $booking->searchBookingsForCalendar($booking);
+$counter = $bookings->rowCount();
+if ($counter > 0) {
+    while ($row = $bookings->fetch()) {
+        extract($row);
+        if (!is_null($dateForth)) {
+            //On donne à $dateForth le format YYYY-MM-DD
+            $dateForth =  implode('-', array_reverse(explode('/', $dateForth)));
+            //$durationDelayInQuarters correspond au nombre de quart d'heure que prend la presta
+            // $durationDelayInQuarters = round(($durationForth+20)/15);
+            // for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
+            //     $calendar[$dateForth][date('H:i', strtotime($hoursForth.' +'.$i*15.' minutes'))] -= 1;
+            // }
+            $dayKey = array_search($dateForth, array_column($calendar, 'dateBookingCalendar'));
+            $hourKey = array_search($hoursForth.'-'.$dateForth, $calendar[$dayKey]);
+            $calendar[$dayKey][$hourKey] = 'Réservé';
+        if (!is_null($dateBack)) {
+            $dateBack =  implode('-', array_reverse(explode('/', $dateBack)));
+            // $durationDelayInQuarters = round(($durationBack+20)/15);
+            // for ($i = 0; $i <= $durationDelayInQuarters; $i++) {
+            //     $calendar[$dateBack][date('H:i', strtotime($hoursBack.' +'.$i*15.' minutes'))] -= 1;
+            // }
+            $dayKey = array_search($dateBack, array_column($calendar, 'dateBookingCalendar'));
+            $hourKey = array_search($hoursBack.'-'.$dateBack, $calendar[$dayKey]);
+            $calendar[$dayKey][$hourKey] = 'Réservé';
+        }
+    }
+}
 
 if (isset($calendar) && !empty($calendar)) {
     echo json_encode($calendar);
